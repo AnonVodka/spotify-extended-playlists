d9cc67b09f5d875d13a5120dfefe38f1c4c2b5ef
AnonVodka <30382084+AnonVodka@users.noreply.github.com>
2022-10-04 10:09:04 +0200

    Update auth.py

--- src/auth.py
+++ src/auth.py
@@ -15,8 +15,8 @@ class SpotifyOAuthHandler(SpotifyOAuth):
             cache_path=cache_path 
         )
 
-        token = self.get_cached_token().get("access_token", None)
-        if token == None:
+        token = self.get_cached_token()
+        if token == None:            
             self.start_webserver()
 
     def get_token(self) -> str:
@@ -30,11 +30,11 @@ class SpotifyOAuthHandler(SpotifyOAuth):
         def index():
                 
             url = request.url
-            code = self.oauth.parse_response_code(url)
+            code = self.parse_response_code(url)
 
             if code != url:
                 print("[#] Found Spotify auth code in Request URL! Trying to get valid access token...")
-                access_token = self.oauth.get_access_token(code, False)
+                access_token = self.get_access_token(code, False)
                 if access_token:
                     print("[#] Access token available! Please restart the application")
                     os._exit(1)
@@ -47,7 +47,7 @@ class SpotifyOAuthHandler(SpotifyOAuth):
             return htmlLoginButton
 
         def getSPOauthURI():
-            auth_url = self.oauth.get_authorize_url()
+            auth_url = self.get_authorize_url()
             return auth_url
 
         print(f"[#] Running web server on {self.ip}:{self.port}..")
